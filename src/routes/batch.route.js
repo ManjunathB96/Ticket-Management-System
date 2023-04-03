15855b77c463d1c64a1f25e1ff1ce7bc6df31391
ManjunathB96 <bbelagavi6@gmail.com>
2023-04-03 23:56:00 +0530

    [Manjunath S B] Added :Refactored

--- src/routes/batch.route.js
+++ src/routes/batch.route.js
@@ -1,19 +1,19 @@
-// import express from 'express';
-// import * as batchController from '../controllers/batch.controller';
-// import { newBatchValidator } from '../validators/batch.validator';
-// import { userAuth } from '../middlewares/auth.middleware';
-// const router = express.Router();
-// import { roleCheck } from '../middlewares/roleCheck.middleware';
+import express from 'express';
+import * as batchController from '../controllers/batch.controller';
+import { newBatchValidator } from '../validators/batch.validator';
+import { userAuth } from '../middlewares/auth.middleware';
+const router = express.Router();
+import { roleCheck } from '../middlewares/roleCheck.middleware';
 
-// //route to all batches
-// router.get('', userAuth, batchController.getAll);
+//route to all batches
+router.get('', userAuth, batchController.getAll);
 
-// //route to create a new batch
-// router.post('',newBatchValidator,userAuth,roleCheck,batchController.createNewBatch);
+//route to create a new batch
+router.post('',newBatchValidator,userAuth,roleCheck,batchController.createNewBatch);
 
-// //route for  add new Engineer
-// router.put('/addEngineers',userAuth,roleCheck,batchController.addNewEngineer);
+//route for  add new Engineer
+router.put('/addEngineers',userAuth,roleCheck,batchController.addNewEngineer);
 
 
 
-// export default router;
+export default router;
